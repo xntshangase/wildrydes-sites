7bfb4df4dd8de235ffdbd31f1647cf0ebe42499d
xntshangase <84807897+xntshangase@users.noreply.github.com>
2024-12-22 15:21:28 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'af-south-1_cHNGrAjOz', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1ss5i0ubs7bnecp5vmdemraq5c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'af-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
